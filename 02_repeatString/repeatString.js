42638c7bf7664153131b941f6398c83ddb2615da
CamiloAHA <caa96ha@gmail.com>
2022-05-12 21:05:20 -0500

    01 and 02 exercises finalized

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, repeats) {
+    let result = ''
+    if (repeats<0) result = 'ERROR'
+    while(repeats>0){
+        result += phrase
+        repeats--
+    }
+    return result
 };
 
 // Do not edit below this line
