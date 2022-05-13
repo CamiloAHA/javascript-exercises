0e3fc7fbfb2fdffbb6d24cc4c5c8ca1401750e58
CamiloAHA <caa96ha@gmail.com>
2022-05-12 22:26:59 -0500

    03 and 04 exercises finalized

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (phrase) {
+    let result = ''
+    let phraseLen = phrase.length
+    while (phraseLen >= 0) {
+        result += phrase.slice(phraseLen, phraseLen + 1)
+        phraseLen--
+    }
 
+    return result
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arrayInput, ...argsToDelete) {
+    let result = arrayInput
+    for (let argumentsLen = 0; argumentsLen < argsToDelete.length; argumentsLen++) {
+        let selectArg = argsToDelete[argumentsLen]
+        for (let i = 0; i < arrayInput.length; i++)
+            if (arrayInput[i] === selectArg) result.splice(i, 1)
+    }
+    return result
 };
 
 // Do not edit below this line
