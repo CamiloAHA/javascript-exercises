f42b4c01e1d86f75f426325d800aabcd32fd96fd
CamiloAHA <caa96ha@gmail.com>
2022-06-17 10:44:39 -0500

    Finish 10, 11, and 12 exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (+num > 0) {
+        let firstNum = 0, secondNum = 1, nextNum
+        for (let i = 0; i < +num; i++) {
+            nextNum = firstNum + secondNum
+            firstNum = secondNum
+            secondNum = nextNum
+        }
+        return firstNum
+    } else {
+        return 'OOPS'
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+    return persons.reduce((oldest, person) => {
+        if (person.yearOfDeath === undefined) person.yearOfDeath = new Date().getFullYear()
+        return oldest.yearOfDeath - oldest.yearOfBirth > person.yearOfDeath - person.yearOfBirth ? oldest : person
+    }, { yearOfDeath: 0, yearOfBirth: 0 })
 };
 
 // Do not edit below this line
