1ade1fee9d16a8f8380090b38fda1102875feae3
CamiloAHA <caa96ha@gmail.com>
2022-06-16 21:52:06 -0500

    Finish 08 and 09 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (obj) {
+  return obj.reduce((count, num) => count + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (obj) {
+  return obj.reduce((count, num) => count * num, 1)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  return a === 1 || a === 0 ? 1 : (a * factorial(a - 1))
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let finalString = phrase.replace(/[!,. ]/g,'').toLowerCase()
+    let copy = finalString
+    for (let i = 0; i < finalString.length; i++) {
+        if (finalString.slice(i, i + 1) === copy.slice(-1)) copy = copy.slice(0, -1)
+        else return false
+    }
+    return true
 };
 
 // Do not edit below this line
